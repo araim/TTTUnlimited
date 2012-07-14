59b0005fae193f2fe4c35eb7217c8329abdcd84a
Jakub Miara <araim@araim.net>
2012-07-14 19:29:22 +0200

    Added hashcode and equals for test purposes

--- TicTacToeUnlimited/src/net/araim/tictactoe/testutils/TestParcelable.java
+++ TicTacToeUnlimited/src/net/araim/tictactoe/testutils/TestParcelable.java
@@ -23,4 +23,29 @@ public class TestParcelable implements Serializable {
 		return s;
 	}
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((s == null) ? 0 : s.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		TestParcelable other = (TestParcelable) obj;
+		if (s == null) {
+			if (other.s != null)
+				return false;
+		} else if (!s.equals(other.s))
+			return false;
+		return true;
+	}
+
 }
